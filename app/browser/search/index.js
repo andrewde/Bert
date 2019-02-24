4b981500c3239273a514365ae3da501ecc2e0e2f
Detiffe <detiffe@amazon.com>
2019-02-24 14:07:50 -0800

    Move event names to const to document event and add logs

--- app/browser/search/index.js
+++ app/browser/search/index.js
@@ -9,12 +9,17 @@ require('../../utils/notifier').initInRenderer()
 const ui = require('./js/ui')
 const shortcutMgr = require('./js/shortcutMgr')
 const { resizeWindow } = ui
+// The keyup event is fired when a key is released.
+const keyup = 'keyup';
+const keydown = 'keydown';
 
 let curItems = []
 let lastCmd = ''
 
 function onExec(cmd) {
+  console.log('onExec', cmd);
   if (cmd !== lastCmd) {
+    console.log('sending exec with cmd', cmd);
     ipcRenderer.send('exec', { cmd })
     lastCmd = cmd
   }
@@ -33,6 +38,7 @@ function onExecItem($select, cmd) {
 }
 
 function onEnter($inp, cmd) {
+  console.log('onEnter');
   if (cmd === lastCmd) {
     let $select = $('.js-item.select');
     if (!$select) {
@@ -45,15 +51,17 @@ function onEnter($inp, cmd) {
 }
 
 function bindInputKeyUp() {
-  $('#search-input').addEventListener('keyup', e => {
+  $('#search-input').addEventListener(keyup, e => {
     onExec(e.target.value)
   })
 }
 
 function bindDocKeyUp() {
-  $.on('keyup', (e) => {
+  $.on(keyup, (e) => {
+    console.log('onEnter');
     const $inp = $('#search-input')
     const cmd = $inp.value
+    // TODO get rid of chinese characters all over the place
     if (e.altKey && e.keyCode >= 49 && e.keyCode <= 57) { // 输入数字
       const index = e.keyCode - 49
       const $select = $$('.js-item')[index]
@@ -72,7 +80,7 @@ function bindDocKeyUp() {
     }
   })
 
-  $.on('keydown', e => shortcutMgr.handleKeyDown(e))
+  $.on(keydown, e => shortcutMgr.handleKeyDown(e))
 }
 // TODO: exec on click item
 function bindItemClick() {
@@ -83,6 +91,7 @@ function bindItemClick() {
 
 function bindIpcEvents() {
   ipcRenderer.on('exec-reply', (event, items) => {
+    console.log('received exec-reply');
     curItems = items
     ui.renderItems(items)
     resizeWindow()
