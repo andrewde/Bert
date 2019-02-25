8ae3fa11ac116b69fdb5a0436ae9a69167be242c
Detiffe <detiffe@amazon.com>
2019-02-25 11:59:52 -0800

    Add more debug

--- app/constants/index.js
+++ app/constants/index.js
@@ -4,6 +4,10 @@ const debug = process.argv.some(value => value.includes('--debug'))
 const dataPath = `${os.homedir()}/.berth`
 const userConfigFile = `${dataPath}/config.json5`
 
+console.log(`debug is ${debug}`);
+console.log(`dataPath is ${dataPath}`);
+console.log(`userConfigFile is ${userConfigFile}`);
+
 module.exports = {
   debug,
   dataPath,
