f6df98b86af8e940933f82889e2c7e5e80bf5926
Detiffe <detiffe@amazon.com>
2019-02-26 15:25:26 -0800

    Tweaked checkstyle and fixed checkstyle for spec

--- app/plugins/enso/index.spec.js
+++ app/plugins/enso/index.spec.js
@@ -1,49 +1,53 @@
-import { expect } from "chai"
-import { setConfig, exec } from "./index"
-// import { fs } from 'fs-extra'
-var fs = require('fs-extra')
+import { expect } from 'chai';
+import { setConfig, exec } from './index';
+// TODO use import as below instead of require
+// import * as fs from 'fs-extra'
+const fs = require('fs-extra');
 import * as sinon from 'sinon';
 
-describe("index test", () => {
+describe('index test', () => {
     let sandbox;
 
     beforeEach(() => {
-        // sandbox = sinon.sandbox.create();
-        let pluginConfig = {};
-        let globalConfig = {};
-        setConfig(pluginConfig, globalConfig)
+        sandbox = sinon.sandbox.create();
+        const pluginConfig = {};
+        const globalConfig = {};
+        setConfig(pluginConfig, globalConfig);
     });
 
     afterEach(() => {
-        // sandbox.restore();
+        sandbox.restore();
     });
 
-    describe("sayHello function", () => {
-        it("should say Hello guys!", () => {
-            // TODO should not be my user directory
-            let expectedFilePath = `/Users/detiffe/.berth/Enso/testName.webloc`;
-            let args = [ 'testName', 'as', 'uuu' ];
-            let event = {
-                sender : {
-                    send : sinon.stub()
+    describe('sayHello function', () => {
+        it('should say Hello guys!', () => {
+            // TODO should not be my user directory, use the OS thing
+            const expectedFilePath = '/Users/detiffe/.berth/Enso/testName.webloc';
+            const args = ['testName', 'as', 'uuu'];
+            const event = {
+                sender: {
+                    send: sinon.stub()
                 }
             };
-            let cmdInfo = { key: 'learn',
-            path: '/workspaces/Berthe/app/plugins/enso/index.js',
-            args: [ 'ppppp', 'as', 'y' ],
-            type: undefined,
-            plugin:
-             { path: '/workspaces/Berthe/app/config/../plugins/enso/index.js',
-               commands: { learn: {}, open: {} },
-               name: 'e',
-               config: {} },
-            config: {} };
+            const cmdInfo = {
+                key: 'learn',
+                path: '/workspaces/Berthe/app/plugins/enso/index.js',
+                args: ['ppppp', 'as', 'y'],
+                type: undefined,
+                plugin:
+                {
+                    path: '/workspaces/Berthe/app/config/../plugins/enso/index.js',
+                    commands: { learn: {}, open: {} },
+                    name: 'e',
+                    config: {}
+                },
+                config: {}
+            };
             exec(args, event, cmdInfo);
             expect(fs.existsSync(expectedFilePath)).to.be.true;
             fs.unlink(expectedFilePath, () => {
-                console.log('in callback');
                 expect(fs.existsSync(expectedFilePath)).to.be.false;
             });
-        })
-    })
-})
+        });
+    });
+});
