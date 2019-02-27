d3984e7526fbd0edf78e30a0836bbebdc1448c40
Detiffe <detiffe@amazon.com>
2019-02-26 21:36:27 -0800

    Fix linter

--- app/plugins/enso/config.sample.js
+++ app/plugins/enso/config.sample.js
@@ -1,19 +1,19 @@
 // sample configuration in `~/.ELaunch/config.js`
 module.exports = {
-  // other config properties
-  plugins: {
+    // other config properties
+    plugins: {
     // ...
     // config for other plugins
 
     // the path of the plugin
-    script: `${__dirname}/../plugins/enso/index.js`,
-    command:{
-      //key is command, value is custom plugin config for command
-      e: {
-        engine: 'Bing',
-        url: 'https://www.bing.com/search/?q=%s',//search url
-        icon: 'https://cn.bing.com/sa/simg/bing_p_rr_teal_min.ico' //icon
-      }
+        script: `${__dirname}/../plugins/enso/index.js`,
+        command: {
+            // key is command, value is custom plugin config for command
+            e: {
+                engine: 'Bing',
+                url: 'https://www.bing.com/search/?q=%s', // search url
+                icon: 'https://cn.bing.com/sa/simg/bing_p_rr_teal_min.ico' // icon
+            }
+        }
     }
-  }
-}
+};
