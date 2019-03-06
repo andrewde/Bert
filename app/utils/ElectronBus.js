9b95cd0da1d75ccbc4d03d7191179b93d6c2a954
Detiffe <detiffe@amazon.com>
2019-03-06 15:48:42 -0800

    Enable lint of all js files under utils and auto-fixed some of the issues

--- app/utils/ElectronBus.js
+++ app/utils/ElectronBus.js
@@ -4,69 +4,69 @@
  * @preserve
  */
 
-const EventEmitter = require('events')
-const electron = require('electron')
+const EventEmitter = require('events');
+const electron = require('electron');
 
 function sendToAllWindows(...args) {
-  electron.BrowserWindow.getAllWindows()
-    .forEach(win => win.webContents.send(...args))
+    electron.BrowserWindow.getAllWindows()
+        .forEach(win => win.webContents.send(...args));
 }
 
 module.exports = class ElectronBus extends EventEmitter {
-  constructor(name='default') {
-    super()
-    this._name = name
-    this._ipcChannel = `EventBus-${name}:`
-    this._listenersMap = {}
+    constructor(name = 'default') {
+        super();
+        this._name = name;
+        this._ipcChannel = `EventBus-${name}:`;
+        this._listenersMap = {};
 
 
-    const ipcMain = electron.ipcMain
-    if (ipcMain) {
-      ipcMain.on(this._ipcChannel, (e, eventJson) => {
-        const eventData = JSON.parse(eventJson)
-        sendToAllWindows(this._ipcChannel, eventJson)
-      })
-    }
-
-    const ipc = electron.ipcMain || electron.ipcRenderer
-    ipc.on(this._ipcChannel, (e, eventJson) => {
-      const eventData = JSON.parse(eventJson)
-      const listeners = this._listenersMap[eventData.event] || []
-      listeners.forEach(listener => {
-        try {
-          listener(...eventData.args)
-        } catch (e) {
-          console.error(e)
+        const ipcMain = electron.ipcMain;
+        if (ipcMain) {
+            ipcMain.on(this._ipcChannel, (e, eventJson) => {
+                const eventData = JSON.parse(eventJson);
+                sendToAllWindows(this._ipcChannel, eventJson);
+            });
         }
-      })
-    })
-  }
 
-  on(event, listener) {
-    this._listenersMap[event] = this._listenersMap[event] || []
-    this._listenersMap[event].push(listener)
-    super.on(event, listener)
-  }
-
-  emit(event, ...args) {
-    const remote = electron.remote
-    const ipcRenderer = electron.ipcRenderer
-    const ipcArgs = [this._ipcChannel, JSON.stringify({
-      event, args: Array.from(args)
-    })]
-
-    if (electron.ipcRenderer) { // renderer process
-      ipcRenderer.send(...ipcArgs)
-    } else { // main process
-      sendToAllWindows(...ipcArgs)
-      super.emit(event, ...args)
+        const ipc = electron.ipcMain || electron.ipcRenderer;
+        ipc.on(this._ipcChannel, (e, eventJson) => {
+            const eventData = JSON.parse(eventJson);
+            const listeners = this._listenersMap[eventData.event] || [];
+            listeners.forEach(listener => {
+                try {
+                    listener(...eventData.args);
+                } catch (e) {
+                    console.error(e);
+                }
+            });
+        });
     }
-  }
 
-  removeListener(event, listener) {
-    super.removeListener(event, listener)
-    if (this._listenersMap[event]) {
-      this._listenersMap[event] = this._listenersMap[event].filter(l => l !== listender)
+    on(event, listener) {
+        this._listenersMap[event] = this._listenersMap[event] || [];
+        this._listenersMap[event].push(listener);
+        super.on(event, listener);
     }
-  }
-}
+
+    emit(event, ...args) {
+        const remote = electron.remote;
+        const ipcRenderer = electron.ipcRenderer;
+        const ipcArgs = [this._ipcChannel, JSON.stringify({
+            event, args: Array.from(args)
+        })];
+
+        if (electron.ipcRenderer) { // renderer process
+            ipcRenderer.send(...ipcArgs);
+        } else { // main process
+            sendToAllWindows(...ipcArgs);
+            super.emit(event, ...args);
+        }
+    }
+
+    removeListener(event, listener) {
+        super.removeListener(event, listener);
+        if (this._listenersMap[event]) {
+            this._listenersMap[event] = this._listenersMap[event].filter(l => l !== listender);
+        }
+    }
+};
--- app/utils/notifier.js
+++ app/utils/notifier.js
@@ -1,27 +1,27 @@
-const electron = require('electron')
-const isRenderer = require('is-electron-renderer')
-const ElectronBus = require('./ElectronBus')
+const electron = require('electron');
+const isRenderer = require('is-electron-renderer');
+const ElectronBus = require('./ElectronBus');
 
-const notifierBus = new ElectronBus('notifier')
+const notifierBus = new ElectronBus('notifier');
 
 function notify(title, options) {
-  return new Notification(title, options || { body: title })
+    return new Notification(title, options || { body: title });
 }
 
 
 module.exports = {
-  initInRenderer() {
-    notifierBus.on('notify', function (title, options) {
-      console.log('notify', title, options)
-      notify.call(this, title, options)
-    })
-    return this
-  },
-  notify(title, options) {
-    if (isRenderer) { // in renderer process
-      notify(title, options)
-    } else {
-      notifierBus.emit('notify', ...arguments)
+    initInRenderer() {
+        notifierBus.on('notify', function (title, options) {
+            console.log('notify', title, options);
+            notify.call(this, title, options);
+        });
+        return this;
+    },
+    notify(title, options) {
+        if (isRenderer) { // in renderer process
+            notify(title, options);
+        } else {
+            notifierBus.emit('notify', ...arguments);
+        }
     }
-  },
-}
+};
