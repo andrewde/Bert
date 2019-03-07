960b57f813491abc54d5343ff51a58d6b02659b9
Detiffe <detiffe@amazon.com>
2019-03-06 16:50:15 -0800

    Add config and auto fix most listing errors

--- app/config/configWatcher.js
+++ app/config/configWatcher.js
@@ -1,81 +1,81 @@
-const electron = require('electron')
-const i18n = require('../i18n')
-const dotDrop = require('dot-prop')
-const { fallbackLng } = require('../constants')
-const promisify = require('../utils/promisify')
-const winMgr = require('../main/winMgr')
-const autoLaunch = require('../main/autoLaunch')
+const electron = require('electron');
+const i18n = require('../i18n');
+const dotDrop = require('dot-prop');
+const { fallbackLng } = require('../constants');
+const promisify = require('../utils/promisify');
+const winMgr = require('../main/winMgr');
+const autoLaunch = require('../main/autoLaunch');
 
-let config
-let rawConfig
+let config;
+let rawConfig;
 
 function setLanguage(ln, update = false) {
-  console.log('set language', ln)
-  if (ln === i18n.language) return
-  i18n.changeLanguage(ln, err => {
-    if (err && ln.includes('-')) {
-      const ln2 = ln.match(/^([^-]+)-/)[1]
-      setLanguage(ln2, true)
-    }
-    if (!update) return
-    if (err) {
-      config.set('language', fallbackLng)
-    } else {
-      config.set('language', ln)
-    }
-  })
+    console.log('set language', ln);
+    if (ln === i18n.language) return;
+    i18n.changeLanguage(ln, err => {
+        if (err && ln.includes('-')) {
+            const ln2 = ln.match(/^([^-]+)-/)[1];
+            setLanguage(ln2, true);
+        }
+        if (!update) return;
+        if (err) {
+            config.set('language', fallbackLng);
+        } else {
+            config.set('language', ln);
+        }
+    });
 }
 
 function syncOnSet(key, value) {
-  const oldRawConfig = config.merge({}, rawConfig)
-  dotDrop.set(rawConfig, key, value)
-  switch (key) {
+    const oldRawConfig = config.merge({}, rawConfig);
+    dotDrop.set(rawConfig, key, value);
+    switch (key) {
     case 'language':
-      setLanguage(value)
-      break;
+        setLanguage(value);
+        break;
     case 'display':
     case 'position':
     case 'position.x':
     case 'position.y':
-      if (!config.isRenderer) {
-        const mainWin = config.context.mainWindow
-        winMgr.setPosition(mainWin, rawConfig.position)
-      }
-      break;
+        if (!config.isRenderer) {
+            const mainWin = config.context.mainWindow;
+            winMgr.setPosition(mainWin, rawConfig.position);
+        }
+        break;
     case 'width':
     case 'maxHeight':
-      if (!config.isRenderer) {
-        const mainWin = config.context.mainWindow
-        winMgr.setContentSize(mainWin, config.width, config.maxHeight)
-      }
-      break;
+        if (!config.isRenderer) {
+            const mainWin = config.context.mainWindow;
+            winMgr.setContentSize(mainWin, config.width, config.maxHeight);
+        }
+        break;
     case 'autoLaunch':
-      if (!config.isRenderer) {
-        autoLaunch(value)
-      }
-      break;
+        if (!config.isRenderer) {
+            autoLaunch(value);
+        }
+        break;
     default:
-  }
+    }
 }
 
 function syncOnInit() {
-  if (rawConfig.language) {
-    setTimeout(() => { // waiting for react load to trigger event
-      setLanguage(rawConfig.language)
-    })
-  }
-  if (config.isFreshInstalled) {
-    autoLaunch(rawConfig.autoLaunch)
-  }
+    if (rawConfig.language) {
+        setTimeout(() => { // waiting for react load to trigger event
+            setLanguage(rawConfig.language);
+        });
+    }
+    if (config.isFreshInstalled) {
+        autoLaunch(rawConfig.autoLaunch);
+    }
 }
 
 module.exports = {
-  init(_config) {
-    config = _config
-    winMgr.init(config)
-    rawConfig = config.getRawConfig()
-    // notify all process to change config
-    config.on('set-config', syncOnSet)
-    syncOnInit()
-  },
-}
+    init(_config) {
+        config = _config;
+        winMgr.init(config);
+        rawConfig = config.getRawConfig();
+        // notify all process to change config
+        config.on('set-config', syncOnSet);
+        syncOnInit();
+    }
+};
--- app/config/index.js
+++ app/config/index.js
@@ -1,110 +1,110 @@
-const os = require('os')
-const fs = require('fs-extra')
-const JSON5 = require('json5')
-const isRenderer = require('is-electron-renderer')
-const electron = require('electron')
-const dotDrop = require('dot-prop')
-const { merge } = require('../utils/merge')
-const ElectronBus = require('../utils/ElectronBus')
-const notifier = require('../utils/notifier')
-const defaultConfig = require('./config.default.js')
-const configWatcher = require('./configWatcher')
-const i18n = require('../i18n')
-const { debug, dataPath, userConfigFile } = require('../constants')
+const os = require('os');
+const fs = require('fs-extra');
+const JSON5 = require('json5');
+const isRenderer = require('is-electron-renderer');
+const electron = require('electron');
+const dotDrop = require('dot-prop');
+const { merge } = require('../utils/merge');
+const ElectronBus = require('../utils/ElectronBus');
+const notifier = require('../utils/notifier');
+const defaultConfig = require('./config.default.js');
+const configWatcher = require('./configWatcher');
+const i18n = require('../i18n');
+const { debug, dataPath, userConfigFile } = require('../constants');
 
-let config = new ElectronBus('config')
+let config = new ElectronBus('config');
 
-let rawConfig = {}
+let rawConfig = {};
 
-let isFreshInstalled = false
+let isFreshInstalled = false;
 
 // A frozen object can no longer be changed.
-Object.freeze(defaultConfig)
+Object.freeze(defaultConfig);
 
 function writeConfig() {
-  fs.outputFileSync(userConfigFile, JSON5.stringify(rawConfig, null, 2), 'utf8')
+    fs.outputFileSync(userConfigFile, JSON5.stringify(rawConfig, null, 2), 'utf8');
 }
 
 function writeDefaultConfig() {
-  try {
-    rawConfig = merge({}, defaultConfig)
-    writeConfig()
-  } catch (err) {
-    console.error(err)
-  }
+    try {
+        rawConfig = merge({}, defaultConfig);
+        writeConfig();
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 function loadConfig() {
-  console.log(`loading config file ${userConfigFile}`);
-  const exist = fs.existsSync(userConfigFile)
-  if (!exist) {
-    writeDefaultConfig()
-    isFreshInstalled = true
-  } else {
-    const userConfigStr = fs.readFileSync(userConfigFile, 'utf8')
-    if (userConfigStr.trim().startsWith('module.exports')) {
-      writeDefaultConfig()
+    console.log(`loading config file ${userConfigFile}`);
+    const exist = fs.existsSync(userConfigFile);
+    if (!exist) {
+        writeDefaultConfig();
+        isFreshInstalled = true;
     } else {
-      rawConfig = merge({}, defaultConfig, JSON5.parse(userConfigStr))
+        const userConfigStr = fs.readFileSync(userConfigFile, 'utf8');
+        if (userConfigStr.trim().startsWith('module.exports')) {
+            writeDefaultConfig();
+        } else {
+            rawConfig = merge({}, defaultConfig, JSON5.parse(userConfigStr));
+        }
     }
-  }
-  return this
+    return this;
 }
 
 Object.assign(config, {
-  dataPath,
-  userConfigFile,
-  debug,
-  isRenderer,
-  merge,
-  isFreshInstalled,
-  getRawConfig() {
-    return rawConfig
-  },
-  getDefaultConfig() {
-    return defaultConfig
-  },
-  getCopyedConfig() {
-    return merge({}, rawConfig)
-  },
-  get(key, defaultValue) {
-    return dotDrop.get(rawConfig, key, defaultValue)
-  },
-  write(key, value) {
-    dotDrop.set(rawConfig, key, value)
-    writeConfig()
-  },
-  set(key, value) { // write with emit event
-    const originalVal = this.get(key)
-    try {
-      this.write(key, value)
-      // notify all process to change config
-      config.emit('set-config', key, value)
-      return true
-    } catch (e) {
-      dotDrop.set(rawConfig, key, originalVal)
-      console.error(e)
-      return false
+    dataPath,
+    userConfigFile,
+    debug,
+    isRenderer,
+    merge,
+    isFreshInstalled,
+    getRawConfig() {
+        return rawConfig;
+    },
+    getDefaultConfig() {
+        return defaultConfig;
+    },
+    getCopyedConfig() {
+        return merge({}, rawConfig);
+    },
+    get(key, defaultValue) {
+        return dotDrop.get(rawConfig, key, defaultValue);
+    },
+    write(key, value) {
+        dotDrop.set(rawConfig, key, value);
+        writeConfig();
+    },
+    set(key, value) { // write with emit event
+        const originalVal = this.get(key);
+        try {
+            this.write(key, value);
+            // notify all process to change config
+            config.emit('set-config', key, value);
+            return true;
+        } catch (e) {
+            dotDrop.set(rawConfig, key, originalVal);
+            console.error(e);
+            return false;
+        }
+    },
+    context: {
+        mainWindow: null,
+        notifier,
+        i18n
     }
-  },
-  context: {
-    mainWindow: null,
-    notifier,
-    i18n,
-  },
-})
+});
 
-loadConfig()
+loadConfig();
 
 
 config = new Proxy(config, {
-  get(target, name) {
-    return name in target
-      ? target[name]
-      : rawConfig[name]
-  },
-})
+    get(target, name) {
+        return name in target
+            ? target[name]
+            : rawConfig[name];
+    }
+});
 
-configWatcher.init(config)
+configWatcher.init(config);
 
-module.exports = config
+module.exports = config;
