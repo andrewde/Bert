175bb46fd4c8f491378c437f0a77840d68816c0a
Detiffe <detiffe@amazon.com>
2019-09-10 16:58:59 -0700

    Remove distributable directory as you cannot download a directory through github and the .app file is just a directory

--- distributables/Bert-v1.0.0.app/Contents/Resources/app/app/plugins/app/config.default.js
+++
@@ -1,1 +0,0 @@
-// TODO if empty can be removed?
--- distributables/Bert-v1.0.0.app/Contents/Resources/app/app/plugins/enso/utils/stringMatcher.js
+++
@@ -1,9 +0,0 @@
-export default {
-    patternsMatchText(text, patterns) {
-        if (!text || !patterns) {
-            return false;
-        }
-        const upperCaseText = text.toUpperCase();
-        return patterns.some(pattern => upperCaseText.includes(pattern.toUpperCase()));
-    }
-};
